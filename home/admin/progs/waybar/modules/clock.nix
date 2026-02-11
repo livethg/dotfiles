df587e85478932026227b5235d110f062ddbd470
Thomas Gauthier <thomgau17@gmail.com>
2026-02-11 11:35:29 -0500

    enhance: style inconsistency
    Signed-off-by: Thomas Gauthier <thomgau17@gmail.com>

--- home/admin/progs/waybar/modules/clock.nix
+++ home/admin/progs/waybar/modules/clock.nix
@@ -7,25 +7,8 @@
     settings = {
       main = {
         clock    = {
-          format         = "{0:%a}  {0:%H󰇙%M}  {0:%d}·{0:%m}";
-          tooltip-format = "<tt><small>{calendar}</small></tt>";
-          calendar       = {
-            mode         = "year";
-            mode-mon-col = 3;
-
-            format = {
-              days     = "<span color='#ecc6d9'><b>{}</b></span>";
-              months   = "<span color='#ffead3'><b>{}</b></span>";
-              weekdays = "<span color='#ffcc66'><b>{}</b></span>";
-              today    = "<span color='#ff6699'><b><u>{}</u></b></span>";
-            };
-          };
-
-          actions = {
-            on-click-right = "mode";
-            on-scroll-up   = [ "tz_up" "shift_up" ];
-            on-scroll-down = [ "tz_down" "shift_down" ] ;
-          };
+          format  = "{0:%a}  {0:%H󰇙%M}  {0:%d}·{0:%m}";
+          tooltip = false;
 
           min-length = 20;
           max-length = 20;
